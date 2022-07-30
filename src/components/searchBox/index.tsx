0c124f658aa1351db487a03224a2db084a16f4b4
chenchun <cc604849909@gmail.com>
2022-07-30 22:02:50 +0800

    feat:update search-box-animation

--- src/components/searchBox/index.tsx
+++ src/components/searchBox/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.less';
 const index = () => {
+    const [isFirst,setisFirst] = useState(true)
+    const [isShow,setIsShow] = useState(true)
+    useEffect(()=>{
+        document.addEventListener('click',()=>{
+            setIsShow(false)
+        },true)//捕获阶段设置，确保被输入框CLICK覆盖
+    },[])
     return (
             <label className='searchBox'>
                 <div className='split'></div>
+                <div className={`inout  ${isFirst?'':isShow?'in':'out'}`}
+                onClick={()=>{
+                    setIsShow(true)
+                    setisFirst(false)//防止初刷动画
+                }}>
                 <div className='search-icon'></div>
+                <input type="text" placeholder='全站检索...' />
+                </div>
             </label>
 
     );
